4d89a6080d70ca0b456b125b85a3d58a0735d04c
Burak Mike <64980398+MahykBurak@users.noreply.github.com>
2021-09-10 22:08:29 +0300

    random meal component

---
+++ components/RandomMeals.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <MainMeal v-if='fetchStatus' :meal='randomMeal'/>
+    <Loading v-if='!fetchStatus'/>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name:'RandomMeals',
+  data(){
+    return{
+      randomMeal:null,
+      fetchStatus: false,
+
+    }
+  },
+  computed:{
+    randomMealfunc(){
+      return this.randomMeal
+    }
+
+  },
+  created(){
+   this.fetchRandomMeal()
+  },
+  methods:{
+    async fetchRandomMeal(){
+      const result = await fetch('https://www.themealdb.com/api/json/v1/1/random.php',{
+        method:'GET'
+      }).then(rs => { return rs.json()})
+      this.randomMeal = result.meals[0]
+      this.fetchStatus = true
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
