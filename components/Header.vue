1debe3a20e9641554093803ebd62f3e5ddb4f127
Burak Mike <64980398+MahykBurak@users.noreply.github.com>
2021-09-10 22:08:05 +0300

    added header

---
+++ components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <header class='header'>
+   <nuxt-link to='/' class='mr-auto font-header text-4xl text-logoGradient'>Recipe</nuxt-link>
+    <nuxt-link to='/'>HOME</nuxt-link>
+    <nuxt-link to='search'>
+      <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+    </svg>
+    </nuxt-link>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
